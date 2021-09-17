1bebed266cd14a7d7187360ec7175978fd74aeb5
Caleb <c4leb.johnson@gmail.com>
2021-09-17 14:57:03 -0700

    fix IP

--- client/src/ts/client.ts
+++ client/src/ts/client.ts
@@ -12,7 +12,7 @@ import { planManager } from "./flightPlan";
 
 const host_url = window.location.href.split(":").slice(1,2).join("");
 
-const _ip = process.env.NODE_ENV == "development" ?  host_url + ":8081" : "https://xcnav.moviequote.net:8081";
+const _ip = process.env.NODE_ENV == "development" ?  host_url + ":8081" : "70.90.174.252:8081";
 const socket = io(_ip, {
     withCredentials: true,
     extraHeaders: {
