407f64dcec9c121fae8c81416bb7dddf1f01111d
Caleb <c4leb.johnson@gmail.com>
2021-09-17 13:08:04 -0700

    tweaks

--- server/src/ts/main.ts
+++ server/src/ts/main.ts
@@ -23,8 +23,9 @@ const host_url = window.location.href.split(":").slice(1,2).join("");
 
 const io = new Server(socketServer, {
     cors: {
-        origin: [host_url + ":8081", "https://www.xcnav.com"],
+        origin: [host_url + ":8081", "https://xcnav.com", /\.xcnav\.com$/],
         methods: ["GET", "POST"],
+        allowedHeaders: ["xcNav"],
         credentials: true
     }
 });
